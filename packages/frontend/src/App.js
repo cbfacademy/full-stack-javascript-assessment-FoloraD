247812003a7adb150ce0305e9bfe5e1c2c5a1ed4
FoloraD <fduang@gmail.com>
2023-11-29 22:01:30 +0000

    create simple UI for store list component to display nearby plantain stores

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchComponent from "./components/SearchComponent"
+import StoreListComponent from "./components/StoreListComponent";
 
 function App() {
   return (
     <div className="App">
       <SearchComponent />
+      <StoreListComponent />
     </div>
   );
 }
--- packages/frontend/src/components/StoreListComponent.js
+++ packages/frontend/src/components/StoreListComponent.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const StoreListComponent = () => {
-    return (
-        <div>
-            Store List Component 
-        </div>
-    );
+  return (
+    <div>
+      <h2>Plantain Stores Near Me </h2>
+      <ul>
+        <li>
+          <h3> Vendor Name</h3>
+          <p> Location: </p>
+          <p>Postcode: </p>
+          <p> Plantain Price: </p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default StoreListComponent;
