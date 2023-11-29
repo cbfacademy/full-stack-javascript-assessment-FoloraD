22a4ac8e94b4436b4e14950b4007a809c76d92bf
FoloraD <fduang@gmail.com>
2023-11-29 22:26:19 +0000

    Add comments to SearchComponent file

--- packages/frontend/src/components/SearchComponent.js
+++ packages/frontend/src/components/SearchComponent.js
@@ -11,3 +11,13 @@ const SearchComponent = () => {
 };
 
 export default SearchComponent;
+
+// TODO
+//import axios , useState
+//function to clean user input to match expected. =>trim whitespace & convert to uppercase
+//something to manage user input(state management) 'postcode' 'setPostcode'
+//function to handle & manage the search action triggered by the user
+//GET request to /searchByPostcode
+//success update vendor state
+//if error update vendor state & display error message
+//update input field & button to reflect new functionality
