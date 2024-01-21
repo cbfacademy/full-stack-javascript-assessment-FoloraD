274cb41b2654ef3fd51b620fa91ac7e65ee0ca9b
FoloraD <fduang@gmail.com>
2024-01-21 21:21:05 +0000

    Add googlemaps to map display component

--- packages/frontend/src/components/MapDisplay.js
+++ packages/frontend/src/components/MapDisplay.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import '../styles/MapDisplay.css'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const MapDisplay = () => {
+
+    const  { isLoaded } = useJsApiLoader ({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    });
+
+    const containerStyle = {
+        height: '400px',
+        width: '100%',
+    };
+
+    const center = {
+        lat: 51.509865,
+        lng: -0.118092,
+    };
+
     return (
         <div className='map-container'>
-            <p className='loading-text'>Map loading </p>
+            {isLoaded ? (
+                <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+                >
+
+                </GoogleMap>
+
+            ) : (
+                <p className='loading-text'>Map loading..</p>
+            )}
+            
         </div>
     );
 };
